6e1eef3feabe6c081bbb694eb7ba1600a83560fd
nathanieljsouth <54192915+nathanieljsouth@users.noreply.github.com>
2021-04-29 18:43:50 +1200

    Completed Cell class for grid

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Cell
+    {
+        public bool CurrentlyAlive=false;
+        bool NextGenerationAlive = false;
+
+        public void CalculateNextState(int NumberOfNeighbours)
+        {
+            if (CurrentlyAlive==true)
+            {
+                if ((NumberOfNeighbours == 2) || (NumberOfNeighbours == 3))
+                {
+                    NextGenerationAlive = true;
+                }
+                else
+                {
+                    NextGenerationAlive = false;
+                }
+            }
+            else
+            {
+                if (NumberOfNeighbours == 3)
+                {
+                    NextGenerationAlive = true;
+                }
+                else
+                {
+                    NextGenerationAlive = false;
+                }
+            }
+        }
+        public void ApplyNextState()
+        {
+            CurrentlyAlive = NextGenerationAlive;
+        }
+        
+        
+    }
+}
